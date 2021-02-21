ef29d3c5efc96e04b273956f3520609fad712098
天王州アテネ <qcmc@qcminecraft.com>
2021-02-21 19:32:08 +0800

    临时增加一言

--- controller/showImg.php
+++ controller/showImg.php
@@ -3,11 +3,13 @@
 $testXML = file_get_contents(SYS_PATH."/assets/full.xml");
 $I = new pics\mlc\Interpreter($testXML);
 
+$hitokoto = jsonUrl("https://v1.hitokoto.cn/?max_length=20&c=a&c=b&c=h&c=j");
+
 $uInfo = viewerInfo();
 $I->drawText(":)", 0, 50, 65, "zpix.ttf", "#9565b1");
 $I->drawText("WOW! It works! From ".$uInfo['ip']."\n 你好啊！来自".
     $uInfo['site']['country'].$uInfo['site']['region']."的朋友~\n当前天气".$uInfo['city']['weather']
-    ."，气温".$uInfo['city']['temperature']."°C"
+    ."，气温".$uInfo['city']['temperature']."°C\n「" . $hitokoto['hitokoto'] . "」"
     , 0, 85, 22, "XiaolaiSC-Regular.ttf", "pink");
 $I->drawText("Powered by pic-signature", 500, 190, 14, "975MaruSC-Regular.ttf", "#349e69", 3);
 
