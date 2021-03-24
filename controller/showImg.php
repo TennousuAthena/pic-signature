3bb8094f0009e9f84ff41cd19211bf0a68803565
天王州アテネ <qcmc@qcminecraft.com>
2021-03-25 01:15:36 +0800

    更新签名档内容

--- controller/showImg.php
+++ controller/showImg.php
@@ -1,13 +1,18 @@
 <?php
 
+use pics\mlc\DB;
 use UAParser\Parser;
 
+$db = new DB();
 $testXML = file_get_contents(SYS_PATH."/assets/full.xml");
 $I = new pics\mlc\Interpreter($testXML);
 
 $ua = Parser::create()->parse($_SERVER['HTTP_USER_AGENT']);
 
 $hitokoto = jsonUrl("https://v1.hitokoto.cn/?max_length=20&c=a&c=b&c=h&c=j");
+
+
+$db->insert(213, "123", "qaq");
 
 $uInfo = viewerInfo();
 $I->drawText(":)", 0, 50, 65, "zpix.ttf", "#9565b1");
@@ -16,8 +21,10 @@ $I->drawText("WOW! It works! From ".$uInfo['ip']."\n 你好啊！来自".
     .$ua->os->toString()."上使用". $ua->ua->family  ."浏览器\n当前天气".$uInfo['city']['weather']
     ."，气温".$uInfo['city']['temperature']."°C\n「" . $hitokoto['hitokoto'] . "」"
     , 250, 85, 22, "XiaolaiSC-Regular.ttf", "pink", 2);
-$I->drawText("Powered by pic-signature", 500, 220, 14, "975MaruSC-Regular.ttf", "#349e69", 3);
+$I->drawText("——".$hitokoto['from'],480, 205, 20, "XiaolaiSC-Regular.ttf", "pink", 3);
+$I->drawText("Powered by pic-signature ", 500, 220, 14, "zpix.ttf", "#349e69", 3);
 
 $I->drawText("Processed in " . round(time_float() - TIME_START, 4) . "s with".((memory_get_usage()-MEM_START)/1000000)."MB used ", 500, 20, 10, "975MaruSC-Regular.ttf", "grey", 3);
 
 $I->show();
+
