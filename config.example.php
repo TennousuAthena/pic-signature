72cc05f8b5c20e5d517006042d44c339814a889e
天王州アテネ <qingcaomc@gmail.com>
2021-03-31 01:20:58 +0800

    配置文件优化

--- config.example.php
+++ config.example.php
@@ -4,7 +4,7 @@ define("SYS_PATH", realpath("./")); //请勿更改此选项
 error_reporting(E_ALL ^ E_NOTICE); //设置错误报告
 return [
     "GA"=>[
-        "PROXY_API_ADDR" => "", //GA镜像URL，不知道是什么请留空
+        "PROXY_API_ADDR" => "https://www.google-analytics.com/", //GA镜像URL，不知道是什么请不要修改
         "tid" => "UA-114514-1",
     ]
 ];
